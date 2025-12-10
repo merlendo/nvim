e0050fd16a9fefdad0f0e3a2b373129d421883f3
merlendo <ian.peysson@etu.univ-grenoble-alpes.fr>
2025-12-10 17:40:24 +0100

    Add support for python lsp with ruff and pyright.

--- init.lua
+++ init.lua
@@ -95,7 +95,7 @@ else
 	)
 
 	-- lsp
-	vim.lsp.enable({ "ruff", "lua_ls", "ts_ls", "gopls", "intelephense", "angularls", "biome" })
+	vim.lsp.enable({ "ruff", "pyright", "lua_ls", "ts_ls", "gopls", "intelephense", "angularls", "biome" })
 	map('n', '<leader>lf', function()
 		vim.lsp.buf.format()
 		vim.lsp.buf.code_action({
@@ -110,6 +110,20 @@ else
 	vim.diagnostic.config({
 		virtual_text = { current_line = true },
 		signs = true,
+	})
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+		callback = function(args)
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			if client == nil then
+				return
+			end
+			if client.name == 'ruff' then
+				-- Disable hover in favor of Pyright
+				client.server_capabilities.hoverProvider = false
+			end
+		end,
+		desc = 'LSP: Disable hover capability from Ruff',
 	})
 
 	-- treesitter
